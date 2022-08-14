fb1c0f1591cca7a727525fb49a6f2d8511490586
chananababa <ephong93@gmail.com>
2022-08-14 23:22:52 +0900

    feat: encrypt last consonant

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,6 +2,8 @@
 // 44032 + 초성번호 * 21 * 28 + 중성번호 * 28 + 종성번호
 // ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ 19 가지
 
+import { lastConsonantMap } from './constants';
+
 const BASE = 44032;
 
 const devide = (c: number) => {
@@ -20,11 +22,11 @@ const devide = (c: number) => {
   };
 
   const getMiddleLetter = (c: number) => {
-    return (subtractBase(c) / 28) % 21;
+    return Math.floor(subtractBase(c) / 28) % 21;
   };
 
   const getFirstLetter = (c: number) => {
-    return subtractBase(c) / 21 / 28;
+    return Math.floor(subtractBase(c) / (21 * 28));
   };
 
   return [getFirstLetter(c), getMiddleLetter(c), getLastLetter(c)];
@@ -34,12 +36,30 @@ const encodeCharacter = (
   encryptKey: number[],
   [first, middle, last]: number[],
 ) => {
-  const encodeConsonant = (consonant: number) => {
+  const encodeFirstConsonant = (consonant: number) => {
     // XXX: exception handling
     if (encryptKey) return encryptKey[consonant];
     return consonant;
   };
-  return [encodeConsonant(first), middle, last];
+
+  const encodeLastConsonant = (consonant: number) => {
+    if (consonant < 0) return consonant;
+    const c = lastConsonantMap[consonant];
+    const encoded = [
+      c[0] === -1 ? -1 : encodeFirstConsonant(c[0]),
+      c[1] === -1 ? -1 : encodeFirstConsonant(c[1]),
+    ];
+    for (let i = 0; i < lastConsonantMap.length; i++) {
+      if (
+        lastConsonantMap[i][0] === encoded[0] &&
+        lastConsonantMap[i][1] === encoded[1]
+      ) {
+        return i;
+      }
+    }
+  };
+
+  return [encodeFirstConsonant(first), middle, encodeLastConsonant(last)];
 };
 
 const combine = ([first, middle, last]: number[]) => {
